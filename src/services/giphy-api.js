22c064ac6cdab8b0a1b8665a3adc413dc7fb6cba
Kate Dameron <katerj33@gmail.com>
2019-06-11 22:34:34 -0700

    simplify giphy fetch

--- src/services/giphy-api.js
+++ src/services/giphy-api.js
@@ -1,7 +1,12 @@
 const key = 'mEaFuMZ2jY0KYICyCKznCcr84R85m26E';
 
 export const getGiphs = (searchTerm) => {
-  return fetch(`http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=${key}&limit=20`)
+  return fetch(`http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=${key}&limit=20`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'accept: image/*'
+    }
+  })
     .then(res => res.json())
     .then(giphs => giphs.data);
 };
